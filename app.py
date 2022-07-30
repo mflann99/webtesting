0e9d82dde6890ff8b4c262040fc79f25fdfbb5a7
mflann99 <mdflannery@wisc.edu>
2022-07-30 15:15:43 -0500

    app working
    No pdf reader stuff yet

--- app.py
+++ app.py
@@ -1,24 +1,59 @@
-
-from flask import Flask, request, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    # completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-# Using the below, the popup message appears on the page load of index.html
-# 0x00001000 - This makes the popup appear over the browser window
-@app.route('/')
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    #win32api.MessageBox(0, 'You have just run a python script on the page load!', 'Running a Python Script via Javascript', 0x00001000)
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']#input id
+        new_task = Todo(content=task_content)
 
-#Using the below, the popup message appears when the button is clicked on the webpage.
-#0x00001000 - This makes the popup appear over the browser window
-@app.route('/test')
-def test():
-    #win32api.MessageBox(0, 'You have just run a python script on the button press!', 'Running a Python Script via Javascript', 0x00001000)
-    page = render_template('index.html', company="Hello")
-    return page
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding task"
 
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Problem with deleting"
+
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error updating info"
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
